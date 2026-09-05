d01d365ed08549e4b6f92f22cbc9108e88952b59
Vinit saini <vinitmali0309@gmail.com>
2026-09-05 19:04:32 +0000

    Just Keep Working

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int score;
+score = 650;
+ const int UID = 232323;
+int balance = 5000;
+int _vinitsproject1 = 3000;
+// UID = 123423;
+    cout<<"Welcome to the world of cpp 1"<<endl;
+     cout<<"Welcome to the world of cpp 2"<<endl;
+      cout<<"Welcome to the world of cpp 3"<<endl;
+       cout<<"Welcome to the world of cpp 4"<<endl;
+
+       return 2;
+}
