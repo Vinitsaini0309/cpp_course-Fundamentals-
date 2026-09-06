a4e4aebef429e70f684f79ddb91d09e1722f1ebb
Vinit saini <vinitmali0309@gmail.com>
2026-09-06 18:26:35 +0000

    okk

---
+++ 02_primitives/data.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int teaLeaves = 50;
+    double waterTemprature = 85.99999999999;
+    float priceofTea = 299.99;
+char Teagrade = 'ABcd';
+bool isTeaready = false;
+
+cout<< Teagrade <<endl;
+cout<< waterTemprature <<endl;
+
+return 0;
+
+
+}
